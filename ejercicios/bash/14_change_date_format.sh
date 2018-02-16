47c8ee833f2ec6307396b6a38a51bc74c3deed85
aborja <alvarodavid.borja@aurea.com>
2018-02-16 16:05:34 -0500

    Se agregan comentarios al sh de cambiar formato de fecha

--- ejercicios/bash/14_change_date_format.sh
+++ ejercicios/bash/14_change_date_format.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+# 1. Buscar un texto con una (,) seguido de un digito y un (/), luego cambiarlo por ,0 + el digito
 sed 's/,\([0-9]\/\)/,0\1/' $1 > out1.tmp
+
+# 2. Buscar un texto con un (/) seguido de un digito y un (/), luego cambiarlo por /0 + el digito
 sed 's/\/\([0-9]\/\)/\/0\1/' out1.tmp > out2.tmp
+
+# 3. Buscar un texto con una (,) seguido de dos digitos mas un (/) seguido de dos digitos mas un (/)
+# seguido de cuatro digitos, intercambiar 3 con 1
 sed 's/,\([0-9][0-9]\)\/\([0-9][0-9]\)\/\([0-9][0-9][0-9][0-9]\)/,\3-\2-\1/' out2.tmp > out3.tmp
